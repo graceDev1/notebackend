e5282081a50c152757b9b61c1cbaf703c785a836
graceDev1 <grace.devolop@gmail.com>
2021-07-17 11:29:07 +0200

    add host

--- note/settings.py
+++ note/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'w5yzw_teg+7c(8x=xko&s06q*s51khz7=uh_tgn&s2*=lhtgc='
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://notesapi-heroku.herokuapp.com/'
+    'https://notesapi-heroku.herokuapp.com/',
+    '127.0.0.1'
 ]
 
 
